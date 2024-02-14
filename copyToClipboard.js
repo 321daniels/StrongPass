872c196499357ae8e24672daf82eb0310ada8673
321daniels <82557991+321daniels@users.noreply.github.com>
2024-02-14 18:51:29 -0500

    Update copyToClipboard.js
    added alerts

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -1,11 +1,17 @@
 function copyToClipboard() {
-    // Get input field
     var passwordField = document.getElementById("generatedPassword");
-    passwordField.select();
 
-    // Copy the  text to the clipboard
-    document.execCommand("copy");
+    // Check if there is a password in the password field
+    if (passwordField.value.trim() === "") {
+        alert("No password to copy. Generate a password first.");
+    } else {
+        passwordField.select();
+        passwordField.setSelectionRange(0, 99999); // For mobile devices
 
-    // Alert the user that the password has been copied
-    alert("Password copied to clipboard!");
+        // Copy the password to the clipboard
+        document.execCommand("copy");
+
+        // Alert that the password has been copied
+        alert("Password copied to clipboard!");
+    }
 }
